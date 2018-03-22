c29869ad56366454d450b725d680c2367e420ff9
raquelinformatica <36679047+raquelinformatica@users.noreply.github.com>
2018-03-22 07:24:10 +0100

    Actualizo ejemplo para escribir en el fichero

--- ejemplo-clase-ficheros-vector-structs.c
+++ ejemplo-clase-ficheros-vector-structs.c
@@ -38,5 +38,37 @@ void main() {
 	// Tercer paso: cerrar el fichero
 	fclose(fichero);
 
+	// Guarda un nuevo punto en el vector
+	printf("Introduce x e y del nuevo punto\n");
+	scanf_s("%f %f", &vectorPuntos[npuntos].x, &vectorPuntos[npuntos].y);
+	// Se incrementa el numero de puntos
+	npuntos++;
+
+	// Codigo que aumenta en 0.5 todas las coordenadas x
+	for (i = 0; i < npuntos; i++) {
+		vectorPuntos[i].x = vectorPuntos[i].x + 0.5;
+	}
+
+	// Antes del salir del programa, hay que grabar los datos en 
+	// el fichero
+
+	// Primer paso: abrir el fichero en modo escritura
+	error = fopen_s(&fichero, "puntos.txt", "w");
+
+	if (error == 0) {
+		printf("Fichero abierto correctamente\n");
+	}
+	else {
+		printf("Ha habido un error %d al abrir el fichero\n", error);
+		return; // return sale del programa
+	}
+	// Segundo paso: escribir los datos en el fichero
+	fprintf(fichero, "%d\n", npuntos);
+
+	for (i = 0; i < npuntos; i++) {
+		fprintf(fichero, "%f\t%f\n", vectorPuntos[i].x, vectorPuntos[i].y);
+	}
+	// Tercer paso: cerrar el fichero
+	fclose(fichero);
 
 }
